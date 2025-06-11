8919bff90ad567c6abd4949f0fe65af7ec19262a
fres12 <fresnelsiregar11@gmail.com>
2025-06-11 12:38:25 +0700

    fix: profil community

--- resources/views/profilcommunity.blade.php
+++ resources/views/profilcommunity.blade.php
@@ -9,7 +9,7 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
-<body class="mb-20 bg-gray-50 min-h-screen">
+<body class="mb-20 min-h-screen" style="background-color: #FDFEFE;">
     @include('components.navbar')
 
     {{-- Profile Section --}}
@@ -56,13 +56,13 @@
         {{-- Tabs + Content --}}
         <div x-data="{ tab: 'all' }">
             {{-- Tabs --}}
-            <div class="flex gap-16 mb-8 justify-center">
+            <div class="flex gap-16 mb-8 justify-center relative">
                 <button @click="tab = 'all'"
                     class="relative pb-3 font-medium text-gray-500 hover:text-gray-900 transition-colors"
                     :class="tab === 'all' ? 'text-gray-900' : ''">
                     All
                     <div x-show="tab === 'all'" x-transition
-                        class="absolute bottom-0 left-1/2 -translate-x-1/2 h-[4px] w-24"
+                        class="absolute bottom-0 left-1/2 -translate-x-1/2 h-[4px] w-16"
                         style="background-color: #CE1212; border-radius: 9999px;">
                     </div>
                 </button>
@@ -89,7 +89,7 @@
                 <div class="absolute left-0 w-full h-px bg-[#e5e7eb]" style="bottom: 0; z-index: 0;"></div>
             </div>
 
-            {{-- All Campaigns --}}
+            {{-- All --}}
             <div x-show="tab === 'all'" x-transition>
                 <div class="space-y-4">
                     @guest
@@ -98,7 +98,7 @@
                         @forelse ($campaigns as $campaign)
                             @include('components.campaignprofile-item', ['campaign' => $campaign])
                         @empty
-                            <div class="text-center text-gray-400 py-10">Belum ada campaign yang Anda buat.</div>
+                            <div class="text-center text-gray-400 py-10">Tidak ada campaign.</div>
                         @endforelse
                     @endguest
                 </div>
